021079e6962410f788590f7f04dc5d9b524ba351
Daria311 <116948751+Daria311@users.noreply.github.com>
2022-12-31 18:08:20 +0500

    Task 2

---
+++ Laba5Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Laba5prod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine().Replace("-", "");
+
+            if (CheckNumber(number))
+            {
+                Console.WriteLine(Sum(number));
+            }
+            else
+            {
+                Console.WriteLine("Некорректные данные!");
+            }
+        }
+
+        static bool CheckNumber(string n)
+        {
+            bool isNumber = true;
+
+            foreach (char s in n)
+            {
+                if (!char.IsDigit(s))
+                    return false;
+            }
+
+            return isNumber;
+        }
+
+        static double Sum(string n)
+        {
+            double sum = 0;
+
+            foreach (char s in n)
+                sum += char.GetNumericValue(s);
+
+            return sum;
+        }
+    }
+}
