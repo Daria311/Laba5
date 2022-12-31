c2dfbfb81aa7e2fdedd7217a2d6a148d2567fed6
Daria311 <116948751+Daria311@users.noreply.github.com>
2022-12-31 17:21:29 +0500

    Task 2

--- Laba5prod/Program.cs
+++ Laba5prod/Program.cs
@@ -4,6 +4,7 @@ namespace Laba5prod
     class Program
     {
         static void Main()
+        static void Main(string[] args)
         {
             string number = Console.ReadLine();
             CheckNumber(number);
@@ -18,11 +19,40 @@ namespace Laba5prod
                     summa += c - '0';
                 }
                 Console.WriteLine(summa);
+            string number = Console.ReadLine().Replace("-", "");
+
+            if (CheckNumber(number))
+            {
+                Console.WriteLine(Sum(number));
             }
             else
             {
                 Console.WriteLine("Некорректно");
+                Console.WriteLine("Некорректные данные!");
             }
+        }
+
+        static bool CheckNumber(string n)
+        {
+            bool isNumber = true;
+
+            foreach (char s in n)
+            {
+                if (!char.IsDigit(s))
+                    return false;
+            }
+
+            return isNumber;
+        }
+
+        static double Sum(string n)
+        {
+            double sum = 0;
+
+            foreach (char s in n)
+                sum += char.GetNumericValue(s);
+
+            return sum;
         }
     }
 }
